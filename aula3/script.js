61b91e9b1d4ef6c741d98f1e82400a584326bb7a
Matheus Dums <dumss@icloud.com>
2023-12-07 22:20:16 -0300

    first commit

---
+++ aula3/script.js
@@ -0,0 +1,17 @@
+// Solicitar o nome pelo prompt e mostrá-lo em console com uma saudação
+var nome = prompt("Digite o seu nome:");
+  alert("Olá, " + nome + "!");
+ 
+
+// Verificando Números Positivos, Negativos ou Zero: Solicite ao usuário que insira um número e determine se ele é positivo, negativo ou zero.
+
+let numeroE = prompt("Digite um numero:");
+
+if (numeroE > 0) {
+    alert("O numero é um numero positivo.")
+} else if (numeroE   < 0){
+    alert("O numero é um numero negativo.")
+} else {
+   alert("O numero é igual a 0.")
+}
+
