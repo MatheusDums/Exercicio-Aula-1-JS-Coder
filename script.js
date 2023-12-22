aa618e4907e23e174b12835d048e3000cc5a4d2b
Matheus Dums <dumss@icloud.com>
2023-12-21 22:30:42 -0300

    exercicio 3, ciclo no simulador

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-function calcularIMC() {
-  var peso = document.getElementById('peso').value;
-  var altura = document.getElementById('altura').value;
+let soma = 0;
+let contador = 0;
 
-  if (peso === '' || altura === '') {
-    alert("Por favor, preencha todos os campos.");
-    return;
+for (;;) {
+  // (;;) = utilizo como um loop infinito para receber valores até que o usuário insira "sair"
+  let input = prompt("Insira um valor (ou 'sair' para encerrar):");
+
+  if (input.toLowerCase() === 'sair') {
+    break;
   }
 
-  var imc = peso / (altura * altura);
-  var resultado = document.getElementById('resultado');
-  resultado.innerHTML = 'Seu IMC é: ' + imc.toFixed(2);
-}
+  let valor = parseFloat(input);
 
+  if (!isNaN(valor)) {
+    soma += valor;
+    contador++;
+
+    let media = soma / contador;
+
+    alert(`Soma: ${soma}   ///   Média: ${media}`);
+  } else {
+    console.log("insira um valor numérico válido.");
+  }
+}
